ea70796cd09a56a5b03f2768e39b92c4b0a9b686
ncn-nakayama <ncn003003@gmail.com>
2026-05-26 15:11:45 +0900

    アソシエーションの修正

--- db/migrate/20260526042553_create_comments.rb
+++ db/migrate/20260526042553_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.1]
   def change
     create_table :comments do |t|
-      t.text       :content   ,null: false, default: ""
-      t.references :prototype ,null: false, default: "", foreign_key: true 
-      t.references :user      ,null: false, default: "", foreign_key: true 
+      t.text       :content   ,null: false
+      t.references :prototype ,null: false, foreign_key: true 
+      t.references :user      ,null: false, foreign_key: true 
       t.timestamps
     end
   end
