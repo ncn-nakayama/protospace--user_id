aaa79bc1308c89eb37bc45b8e7fcf619a0caaa57
ncn-nakayama <ncn003003@gmail.com>
2026-05-27 16:48:24 +0900

    詳細ページの遷移、表示、コメント機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :show]
   def index
     @prototypes = Prototype.all
   end
@@ -17,12 +18,25 @@ class PrototypesController < ApplicationController
     end
   end
 
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
   def show
-  @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
   end
 
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
  devise_for :users
  root to: "prototypes#index"
  resources :users, only: [:edit, :update]
- resources :prototypes, only: [:index, :new, :create] do
-  resources :comments, only: [:index, :create]
+ resources :prototypes do
+    resources :comments, only: [:create]
  end
 end
