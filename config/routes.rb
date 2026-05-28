70508042b157ff54676b31084903dd3b1036d756
ncn-nakayama <ncn003003@gmail.com>
2026-05-28 11:00:18 +0900

    投稿者以外のユーザーが専用ページに遷移できない設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
  devise_for :users
  root to: "prototypes#index"
- resources :users, only: [:edit, :update, :show]
+ resources :users, only: [:edit, :update]
  resources :prototypes do
     resources :comments, only: [:create]
  end
